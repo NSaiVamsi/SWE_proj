2048543d8cacb5730d414599b6ddf490ea9a08a7
Pranav-Varma-03 <harshapranav323@gmail.com>
2024-04-18 14:51:34 +0530

    1st push

---
+++ Phase 5/backend/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require('mongoose');
+require("dotenv").config();
+
+// const UploadRoute = require("./routes/UploadRoute");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB Connected...");
+    // Your code here, such as starting your server
+  })
+  .catch(err => {
+    console.error("Error connecting to MongoDB:", err);
+  });
+
+// app.use(UploadRoute);
+
+app.get("/",(req,res)=>{
+    res.send("Welcome to Backend");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started at port: ${PORT}`);
+});
